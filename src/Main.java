76ba3eb5bb9e06e985732f293cd0b27aec3e3636
NguyenPhatTai <phattainguyen04w7@gmail.com>
2024-11-29 13:57:11 +0700

    Update print-name

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Nguyen_Phat_Tai's Java Repository.");
+        System.out.println("Hello Nguyen Phat Tai");
+        System.out.println("Welcome to my repository.");
     }
 }
